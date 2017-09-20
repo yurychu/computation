76b57a8068c81f111bda10477312522bd8c3a628
yurychu <yury_chugunov@mail.ru>
2017-09-20 23:03:19 +0300

    ruby basics

---
+++ ruby_hard/ex1.rb
@@ -0,0 +1,6 @@
+puts "Hello Fate!"
+puts "Hello again"
+puts "bla bla bla"
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
---
+++ ruby_hard/ex2.rb
@@ -0,0 +1,8 @@
+#commmmment
+
+puts "I could have code like this."
+
+#puts "This won't run"
+
+puts "This # will run."
+
